665fcd1cac72c20e04a3806c912321ba572ea3a8
kris-veto <82740207+kris-veto@users.noreply.github.com>
2026-04-21 07:05:25 -0400

    Initialize Spring Boot project (Spring Initializr)
    Generated using Spring Initializr. 
    
    Includes: 
    - Spring Boot 4.0.0 
    - Maven project structure 
    - Basic dependencies: Spring Web, Spring Data JPA, MySQL Driver, Spring Boot DevTools 
    - Default package structure 
    
    Initial backend setup for the ems-backend project.

---
+++ ems-backend/src/main/java/com/cvera/ems_backend/EmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cvera.ems_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsBackendApplication.class, args);
+	}
+
+}
